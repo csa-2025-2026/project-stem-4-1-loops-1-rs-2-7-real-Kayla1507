7bbbac5b1dddd93c8afa58d482f6970364ac7659
per5 <klapham007@gmail.com>
2025-10-17 21:46:15 +0000

    finsihed 1

--- Main.java
+++ Main.java
@@ -5,5 +5,29 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+     
+    Scanner scan = new Scanner(System.in);
+    //1
+    /*
+    System.out.println("Enter any numbers (enter -1 to stop)");
+    int num = scan.nextInt();
+    int sum = 0;
+
+    while (num != -1)
+    {
+      sum += num;
+      num = scan.nextInt();
+      
+    }
+    System.out.println(sum); */ 
+
+    //2
+    // Scanner scan = new Scanner(System.in);
+    System.out.println(" how many numbers do u wish to input?");
+    int inputs = scan.nextInt();
+    System.out.println(" Enter ur numbers");
+    int num = scan.nextInt();
+
+    while (inputs < )
   }
 }
